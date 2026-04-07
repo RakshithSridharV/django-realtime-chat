56790eb03eb6c5a82a075fa8bb462170963e60f5
Rakshith <rakshithsridhar05@gmail.com>
2026-04-08 00:58:28 +0530

    fix: import views from chat app

--- djangochat/urls.py
+++ djangochat/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.conf import settings
-from . import views
+from chat import views   # ✅ FIXED
 
 urlpatterns = [
     path('admin/', admin.site.urls),
